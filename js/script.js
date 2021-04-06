2310064a25bc289ec7bf4bd559ac91179332dd48
Dani1kazantsev <61469739+Dani1kazantsev@users.noreply.github.com>
2021-04-07 01:19:33 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function () {
+  $('.menu-open').click(function () {
+    $('.menu').toggleClass('show-menu')
+  })
+    $('.menu-open').click(function () {
+    $('.menu-open').toggleClass('menu-active')
+  })
+});
